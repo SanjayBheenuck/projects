a725b1bcc33dddb41b5c4cb03bed043e74652947
Tom Michiels <tmichiels@gmail.com>
2019-04-16 09:06:09 +0100

    Initial commit

---
+++ src/main/java/m2them/moviemadness/MovieMadnessApplication.java
@@ -0,0 +1,13 @@
+package m2them.moviemadness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieMadnessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieMadnessApplication.class, args);
+    }
+
+}
---
+++ src/test/java/m2them/moviemadness/MovieMadnessApplicationTests.java
@@ -0,0 +1,16 @@
+package m2them.moviemadness;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MovieMadnessApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
